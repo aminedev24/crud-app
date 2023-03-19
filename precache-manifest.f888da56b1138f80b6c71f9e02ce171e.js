c9895b8152bb4d33129a2d0e68668b8d8e083709
aminedev24 <abdoufuntuber@gmail.com>
2023-03-20 00:03:24 +0100

    Updates

---
+++ precache-manifest.f888da56b1138f80b6c71f9e02ce171e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90c886a15ad465ac91a2",
+    "url": "/crud-app/static/css/main.d4b26ccd.chunk.css"
+  },
+  {
+    "revision": "90c886a15ad465ac91a2",
+    "url": "/crud-app/static/js/main.90c886a1.chunk.js"
+  },
+  {
+    "revision": "561126de61029355b316",
+    "url": "/crud-app/static/js/1.561126de.chunk.js"
+  },
+  {
+    "revision": "c285be5d5764258af05f",
+    "url": "/crud-app/static/js/2.c285be5d.chunk.js"
+  },
+  {
+    "revision": "b9dde5bc06c0a321cf38",
+    "url": "/crud-app/static/js/runtime~main.b9dde5bc.js"
+  },
+  {
+    "revision": "60ec569e708c794eb181ed3104f91e75",
+    "url": "/crud-app/index.html"
+  }
+];
